bd2c94309f73b7343d1aff9bf6355aca77b1b74e
Patrycja Filipowicz <patrycja.filipowicz46@gmail.com>
2021-11-16 13:00:13 +0100

    Add new files

---
+++ main.js
@@ -0,0 +1,51 @@
+const form = document.querySelector(".quiz-box");
+
+const answers = Array.from(document.querySelectorAll(".answer"))
+const allQuestions = document.querySelectorAll('.question')
+const modal = document.querySelector(".modal")
+const modalInfo = document.querySelector("p")
+const modalBtn = document.querySelector(".close-modal")
+
+const handleQuiz = e => {
+    e.preventDefault();
+    const checkedAnswers = answers.filter(answer => answer.checked)
+    const isTrue = checkedAnswers.every(answer => answer.value === "true")
+
+    const allChecked = checkedAnswers.length === allQuestions.length
+    
+if(!allChecked) {
+    modal.classList.add("modal-active")
+    modalInfo.textContent = "Wybierz wszystkie odpowiedzi!"
+    return
+}
+
+checkedAnswers.forEach(answer => {
+    const checkIfCorrect = answer.value === "true"
+
+    const answerBox = answer.closest(".answer-box")
+
+    if(checkIfCorrect) {
+        answerBox.classList.add("correct")
+        answerBox.classList.remove("incorrect")
+    } else {
+        answerBox.classList.add("incorrect")
+        answerBox.classList.remove("correct")
+    }
+})
+
+if(isTrue && allChecked) {
+    modal.classList.add("modal-active")
+    modalInfo.textContent = "Brawo wszystkie odpowiedzi są poprawne!"
+} else {
+    modal.classList.add("modal-active")
+    modalInfo.textContent = "Niestety przegrywasz..."
+}
+
+}
+
+const closeModal = () => {
+    modal.classList.remove("modal-active")
+}
+
+modalBtn.addEventListener("click", closeModal)
+form.addEventListener("submit", handleQuiz)
